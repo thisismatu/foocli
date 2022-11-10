4217adcb55552dc528a55d14abda4b5805dcc295
Mathias Lindholm <mathias@speechly.com>
2022-11-10 14:40:51 +0200

    Add alias for projects

--- cmd/projects.go
+++ cmd/projects.go
@@ -7,8 +7,9 @@ import (
 )
 
 var projectsCmd = &cobra.Command{
-	Use:   "projects",
-	Short: "View and manage projects",
+	Use:     "projects",
+	Aliases: []string{"project"},
+	Short:   "View and manage projects",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			cmd.Help()
