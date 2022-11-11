91ccba4ed78c9b6808962146e9717252784df3a8
Mathias Lindholm <mathias@speechly.com>
2022-11-11 23:11:18 +0200

    Add example

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
@@ -11,9 +13,10 @@ var dbProjects = ".cfg/projects.jsonl"
 var dbApplications = ".cfg/applications.jsonl"
 
 var rootCmd = &cobra.Command{
-	Use:   "foo",
-	Short: "Foo CLI",
-	Long:  `Foo is a CLI tool for using the Foo platform`,
+	Use:     "foo",
+	Short:   "Foo CLI",
+	Long:    "Foo is a CLI tool for using the Foo platform",
+	Example: fmt.Sprintf("  Deploy changes\n  %s\n\n  Transcribe audio file\n  %s\n\n  Switch project\n  %s\n", color.CyanString("foo deploy <app_id> /path/to/config"), color.CyanString("foo transcribe <app_id> file.wav"), color.CyanString("foo projects switch")),
 }
 
 func Execute() {
